58798748459d82219d6f9e8ee529955389b47b0e
Jenna Chinn <chinnjenna@gmail.com>
2025-05-14 20:23:21 -0700

    start conversation from message screen

--- backend/public/scripts/messages.js
+++ backend/public/scripts/messages.js
@@ -1,5 +1,23 @@
 import { getCookie } from "./getCookie.js";
 
+const newMessageWindow = document.querySelector(".new-message-popup");
+
+const closeWindow = () => { newMessageWindow.classList.remove("show"); }
+
+document.querySelector(".new-message-button").addEventListener("click", (e) => {
+  if (!newMessageWindow.classList.contains("show")) {
+    e.stopPropagation();
+    newMessageWindow.classList.add("show");
+  }
+});
+
+window.addEventListener("click",closeWindow);
+
+newMessageWindow.addEventListener("click",(e) => {
+  e.stopPropagation();
+});
+
+
 window.addEventListener("load", () => {
     // Get the JWT token from the cookie
     const jwt = getCookie("jwt");
@@ -34,4 +52,28 @@ window.addEventListener("load", () => {
                 });
             });
         });
+    
+    const closeWindowButton = document.createElement("div");
+    closeWindowButton.id = "close-window-button";
+    closeWindowButton.innerHTML = "&#10006;"
+    closeWindowButton.addEventListener("click",closeWindow);
+    newMessageWindow.appendChild(closeWindowButton);
+
+    fetch("/api/friends", {
+      method: "GET",
+      headers,
+    })
+      .then((response) => response.json()
+      .then((data) => {
+        const friends = data.data;
+        friends.forEach((friend) => {
+          var friendDiv = document.createElement("div");
+          friendDiv.className = "friend-div";
+          friendDiv.innerHTML = friend.username;
+          friendDiv.addEventListener("click", () => {
+            window.location.href = `/messages/conversation?user_id=${friend.friend_user_id}`;
+          })
+          newMessageWindow.appendChild(friendDiv);
+        });
+      }));
 })
